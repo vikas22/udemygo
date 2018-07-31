0c0601c30d06d21320e2237980d046403db0039f
Vikas Naidu <vck173@gmail.com>
2018-07-31 16:43:50 +0530

    lesson 1

---
+++ firstapp/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"firstapp/utility"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+	utility.SayHello()
+}
---
+++ firstapp/utility/utility.go
@@ -0,0 +1,11 @@
+package utility
+
+import "fmt"
+
+func SayHello() {
+	fmt.Println("Say Hello!")
+}
+
+func saySomething() {
+	fmt.Println("Say Something")
+}
